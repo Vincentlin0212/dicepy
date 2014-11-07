7067ac2db72af39b23e8e0c9bf80e0aacc7068f7
Vincent Lin <vincentlin0212@gmail.com>
2014-11-07 09:39:43 -0500

    Finish new feature

--- dice.py
+++ dice.py
@@ -6,8 +6,14 @@ from special_input import *
 howMany = int_input("How many dice would you like to roll? >")
 a = 1
 for i in range (howMany):
+	while True:
+		sides = int_input("How many sides on your die #{}? >".format(a))
 	
-	sides = int_input("How many sides on your die #{}? >".format(a))
+		if sides <= 2:
+			print("Sides can't be smaller than 2, try again.")
+			continue
+		else:
+			break
 	number = random.randint(1, sides)
 	print("The die shows: {}".format(number))
 	a += 1
