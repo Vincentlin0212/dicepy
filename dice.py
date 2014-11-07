14b8b1f7c585afd574e7b4d4ed6df824d5399ae6
Vincent Lin <vincentlin0212@gmail.com>
2014-11-07 09:25:34 -0500

    New feature with develop

--- dice.py
+++ dice.py
@@ -2,15 +2,12 @@
 
 import random
 from special_input import *
-"""
-	ask times
-"""
-howMany = int_input("How many dice would you like to roll? >")
-"""
-	ask how many side
-"""
-sides = int_input("How many sides on your die? >")
 
+howMany = int_input("How many dice would you like to roll? >")
+a = 1
 for i in range (howMany):
+	
+	sides = int_input("How many sides on your die #{}? >".format(a))
 	number = random.randint(1, sides)
 	print("The die shows: {}".format(number))
+	a += 1
