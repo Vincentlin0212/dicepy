814ceef2327af1c69744c36e51375bc9d9341b9a
Vincent Lin <vincentlin0212@gmail.com>
2014-11-07 09:15:20 -0500

    Docstrings

--- dice.py
+++ dice.py
@@ -1,8 +1,14 @@
 #! /usr/bin/env python
+
 import random
 from special_input import *
-
+"""
+	ask times
+"""
 howMany = int_input("How many dice would you like to roll? >")
+"""
+	ask how many side
+"""
 sides = int_input("How many sides on your die? >")
 
 for i in range (howMany):
